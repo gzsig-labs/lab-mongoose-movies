47ef558e2f813a8979aec9fdb88490ea75b04135
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-31 21:45:54 -0300

    iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,31 @@
+const {CelebrityModel} = require('../models')
 const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res) => {
+  CelebrityModel
+    .find( { } )
+    .then( 
+      (celebs) => {
+        const prettyCeleb = celebs.map(
+          (celeb)=>{
+            return {
+              id: celeb.id,
+              name: celeb.name,
+              occupation: celeb.occupation,
+              catchPhrase: celeb.catchPhrase
+            }
+          }
+        )
+    res.render("celebrities/index",{prettyCeleb})
+    // res.json(prettyCeleb)
+  }
+  )
 });
 
 module.exports = router;
